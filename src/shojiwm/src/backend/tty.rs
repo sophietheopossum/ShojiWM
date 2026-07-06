db6dca0be240f2765bb65b41b19a689f029b203c
Seirra <sophietheopossum@yandex.ru>
2026-07-06 01:21:12 +0100

    Add HdrEncode render element name to TTY backend

--- src/shojiwm/src/backend/tty.rs
+++ src/shojiwm/src/backend/tty.rs
@@ -2081,6 +2081,7 @@ fn tty_render_element_name(element: &TtyRenderElements) -> &'static str {
         TtyRenderElements::RelocatedBackdrop(_) => "RelocatedBackdrop",
         TtyRenderElements::TransformedBackdrop(_) => "TransformedBackdrop",
         TtyRenderElements::Cursor(_) => "Cursor",
+        TtyRenderElements::HdrEncode(_) => "HdrEncode",
         _ => "Generic",
     }
 }
